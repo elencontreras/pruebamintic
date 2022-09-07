c251058d11faa9e852701cb5d4adf9aa031d3e86
Elen <elencontreras6@gmail.com>
2022-09-06 21:04:24 -0500

    asdddddd

--- src/GeneradorVertebrados.java
+++ src/GeneradorVertebrados.java
@@ -1,7 +1,7 @@
 public class GeneradorVertebrados {
     public static Vertebrado[] generarArrayVertebrados(){
         Vertebrado[] miVertebrados = new Vertebrado[4];
-        miVertebrados[0] = new Vertebrado("Camello", "Mamifero"); 
+        miVertebrados[0] = new Vertebrado("sss", "Mamifero"); 
         miVertebrados[1] = new Vertebrado("Vsssaca", "Mamifero");
         miVertebrados[2] = new Vertebrado("ddddd", "Pez"); 
         
